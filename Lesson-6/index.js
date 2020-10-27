f6dffaefa6adf227b4c0af4fa0bb6b6fc5cd1f0f
Ellen Treidler <71969472+Ellentreidler@users.noreply.github.com>
2020-10-27 21:04:20 +0100

    Lesson 6

---
+++ Lesson-6/index.js
@@ -0,0 +1,340 @@
+/*let str='Привіт';
+let num=123;
+let flag=true;
+let txt='true'
+
+console.log(str);
+alert(num);
+document.write(have123)*/
+
+/*let isAdult=confirm('Are you 18?');
+document.write(isAdult)*/
+
+/*let userName = prompt('What is your name?');
+document.write(userName);*/
+
+
+
+/*task 1*/
+
+/*'hello'*/
+/*let textHello = 'hello';
+console.log(textHello);*/
+
+/*
+let textHello = 'hello';
+alert(textHello);*/
+
+/*
+let textHello = 'hello';
+document.write(textHello);*/
+
+
+
+/*'owu'*/
+/*let textOwu = 'owu';
+console.log(textOwu);*/
+
+/*
+let textOwu = 'owu';
+alert(textOwu);*/
+
+/*let textOwu = 'owu';
+document.write(textOwu);*/
+
+
+/*'com'*/
+/*let textCom = 'com';
+console.log(textCom);*/
+
+/*let textCom = 'com';
+alert(textCom);*/
+
+/*let textCom = 'com';
+document.write(textCom);*/
+
+/*'ua'*/
+/*let textUa = 'ua';
+console.log(textUa);*/
+
+/*let textUa = 'ua';
+alert(textUa);*/
+
+/*let textUa = 'ua';
+document.write(textUa);*/
+
+
+
+/*1*/
+/*let numberOne = 1;
+console.log(numberOne);*/
+
+/*let numberOne = 1;
+alert(numberOne);*/
+
+/*let numberOne = 1;
+document.write(numberOne);*/
+
+/*10*/
+/*let numberTen = 10;
+console.log(numberTen);*/
+
+/*let numberTen = 10;
+alert(numberTen);*/
+
+/*let numberTen = 10;
+document.write(numberTen);*/
+
+
+
+/*-999*/
+/*let numberMinus_nine_hundred_ninety_nine = -999;
+console.log(numberMinus_nine_hundred_ninety_nine);*/
+
+/*let numberMinus_nine_hundred_ninety_nine = -999;
+alert(numberMinus_nine_hundred_ninety_nine);*/
+
+/*let numberMinus_nine_hundred_ninety_nine = -999;
+document.write(numberMinus_nine_hundred_ninety_nine);*/
+
+
+
+/*123*/
+/*let numberOne_hundred_twenty_three = 123;
+console.log(numberOne_hundred_twenty_three);*/
+
+/*let numberOne_hundred_twenty_three = 123;
+alert(numberOne_hundred_twenty_three);*/
+
+/*let numberOne_hundred_twenty_three = 123;
+document.write(numberOne_hundred_twenty_three);*/
+
+
+/*3.14*/
+/*let numberPI = 3.14;
+console.log(numberPI);*/
+
+/*let numberPI = 3.14;
+alert(numberPI);*/
+
+/*let numberPI = 3.14;
+document.write(numberPI);*/
+
+
+
+/*2.7*/
+/*let numberTwo_point_seven = 2.7;
+console.log(numberTwo_point_seven);*/
+
+/*let numberTwo_point_seven = 2.7;
+alert(numberTwo_point_seven);*/
+
+/*let numberTwo_point_seven = 2.7;
+document.write(numberTwo_point_seven);*/
+
+
+/*16*/
+/*let numberSixteen = 16;
+console.log(numberSixteen);*/
+
+/*let numberSixteen = 16;
+alert(numberSixteen);*/
+
+/*let numberSixteen = 16;
+document.write(numberSixteen);*/
+
+
+/*true*/
+/*let DoYouLikeCats =true;
+console.log(DoYouLikeCats);*/
+
+/*let DoYouLikeCats =true;
+alert(DoYouLikeCats);*/
+
+/*let DoYouLikeCats =true;
+document.write(DoYouLikeCats);*/
+
+
+/*false*/
+/*let DoYouLikeCats =false;
+console.log(DoYouLikeCats);*/
+
+/*let DoYouLikeCats =false;
+alert(DoYouLikeCats);*/
+
+/*let DoYouLikeCats =false;
+document.write(DoYouLikeCats);*/
+
+
+/*task 2*/
+/*let textHello = 'hi!';
+console.log(textHello);*/
+
+/*let textHello = 'hi!';
+alert(textHello);*/
+
+/*let textHello = 'hi!';
+document.write(textHello);*/
+
+
+
+/*let textOwu = 'oh no';
+console.log(textOwu);*/
+
+/*let textOwu = 'oh no';
+alert(textOwu);*/
+
+/*let textOwu = 'oh no';
+document.write(textOwu);*/
+
+
+/*let textCom = 'Yes!';
+console.log(textCom);*/
+
+/*let textCom = 'Yes!';
+alert(textCom);*/
+
+/*let textCom = 'Yes!';
+document.write(textCom);*/
+
+
+
+/*let textUa = 'EN';
+console.log(textUa);*/
+
+/*let textUa = 'EN';
+alert(textUa);*/
+
+/*let textUa = 'EN';
+document.write(textUa);*/
+
+
+/*let numberOne = 2;
+console.log(numberOne);*/
+
+/*let numberOne = 2;
+alert(numberOne);*/
+
+/*let numberOne = 2;
+document.write(numberOne);*/
+
+
+/*let numberTen = 11;
+console.log(numberTen);*/
+
+/*let numberTen = 11;
+alert(numberTen);*/
+
+/*let numberTen = 11;
+document.write(numberTen);*/
+
+
+/*let numberMinus_nine_hundred_ninety_nine = -1000;
+console.log(numberMinus_nine_hundred_ninety_nine);*/
+
+/*let numberMinus_nine_hundred_ninety_nine = -1000;
+alert(numberMinus_nine_hundred_ninety_nine);*/
+
+/*let numberMinus_nine_hundred_ninety_nine = -1000;
+document.write(numberMinus_nine_hundred_ninety_nine);*/
+
+
+/*let numberOne_hundred_twenty_three = 200;
+console.log(numberOne_hundred_twenty_three);*/
+
+/*let numberOne_hundred_twenty_three = 200;
+alert(numberOne_hundred_twenty_three);*/
+
+/*let numberOne_hundred_twenty_three = 200;
+document.write(numberOne_hundred_twenty_three);*/
+
+
+/*let numberPI = 3.14159;
+console.log(numberPI);*/
+
+/*let numberPI = 3.14159;
+alert(numberPI);*/
+
+/*let numberPI = 3.14159;
+document.write(numberPI);*/
+
+
+/*let numberTwo_point_seven = 3.1;
+console.log(numberTwo_point_seven);*/
+
+/*let numberTwo_point_seven = 3.1;
+alert(numberTwo_point_seven);*/
+
+/*let numberTwo_point_seven = 3.1;
+document.write(numberTwo_point_seven);*/
+
+
+/*let numberSixteen = 20;
+console.log(numberSixteen);*/
+
+/*let numberSixteen = 20;
+alert(numberSixteen);*/
+
+/*let numberSixteen = 20;
+document.write(numberSixteen);*/
+
+
+/*let DoYouLikeCats =false;
+console.log(DoYouLikeCats);*/
+
+/*let DoYouLikeCats =false;
+alert(DoYouLikeCats);*/
+
+/*let DoYouLikeCats =false;
+document.write(DoYouLikeCats);*/
+
+
+/*false*/
+/*let DoYouLikeCats =true;
+console.log(DoYouLikeCats);*/
+
+/*let DoYouLikeCats =true;
+alert(DoYouLikeCats);*/
+
+/*let DoYouLikeCats =true;
+document.write(DoYouLikeCats);*/
+
+
+/*task 3*/
+/*const DoYouLikeCats =false;
+console.log(DoYouLikeCats);*/
+
+/*const DoYouLikeCats =false;
+alert(DoYouLikeCats);*/
+
+/*const DoYouLikeCats =false;
+document.write(DoYouLikeCats);*/
+
+
+/*const numberSixteen = 20;
+console.log(numberSixteen);*/
+
+/*const numberSixteen = 20;
+alert(numberSixteen);*/
+
+/*const numberSixteen = 20;
+document.write(numberSixteen);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
