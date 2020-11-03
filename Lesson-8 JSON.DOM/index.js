bbea45483aaa066596abbb64ac8a105d716cf55a
Ellen Treidler <71969472+Ellentreidler@users.noreply.github.com>
2020-11-03 23:40:01 +0100

    Lesson 8

---
+++ Lesson-8 JSON.DOM/index.js
@@ -0,0 +1,226 @@
+/*Task - 1*/
+
+// const dog = {
+//     name:'April',
+//     age: 3,
+//     weight: '65 kg',
+//     height: '22cm',
+//     colour: 'any shades of gold or cream',
+//     life: 10
+// }
+// console.log(dog);
+
+// const person = {
+//     name:'Olena',
+//     age: 100,
+//     weight: '65 kg',
+//     height: '200 cm',
+//     house: false,
+//     friend: true
+// }
+// console.log(person);
+
+
+// const car = {
+//     name: ' Ford',
+//     age: 200,
+//     designer: 'Henry Ford',
+//     wheelbase: 100,
+//     length: 134,
+//     width:'1,676 mm'
+// }
+// console.log(car);
+
+// const flat = {
+//     name: 'Gulliver',
+//     type: 'mixed-use',
+//     location: 'Kyiv, Ukraine',
+//     cost:'$200,000,000',
+//     roof: true
+// }
+// console.log(flat);
+
+// const book = {
+//     name: 'Atlas Shrugged',
+//     author: 'Ayn Rand',
+//     language: 'English',
+//     pages: 1.168,
+//     publisher: 'Random House'
+// }
+// console.log(book);
+
+/*Task - 2*/
+/*const dogs = [
+    {name: 'April', age: 3, weight: '65 kg', height: '22cm', colour: 'any shades of gold or cream', life: 10 },
+    {name: 'April', age: 3, weight: '65 kg', height: '22cm', colour: 'any shades of gold or cream', life: 10 },
+    {name: 'April', age: 3, weight: '65 kg', height: '22cm', colour: 'any shades of gold or cream', life: 10 },
+    {name: 'April', age: 3, weight: '65 kg', height: '22cm', colour: 'any shades of gold or cream', life: 10 },
+    {name: 'April', age: 3, weight: '65 kg', height: '22cm', colour: 'any shades of gold or cream', life: 10 }
+]
+console.log(dogs);*/
+
+
+/*const people = [
+    {name: 'Olena', age: 100, weight: '65 kg', height: '200 cm', house: false, friend: true },
+    {name: 'Olena', age: 100, weight: '65 kg', height: '200 cm', house: false, friend: true },
+    {name: 'Olena', age: 100, weight: '65 kg', height: '200 cm', house: false, friend: true },
+    {name: 'Olena', age: 100, weight: '65 kg', height: '200 cm', house: false, friend: true },
+    {name: 'Olena', age: 100, weight: '65 kg', height: '200 cm', house: false, friend: true }
+]
+console.log(people);*/
+
+/*const cars = [
+    {name: 'Ford', age: 200, designer: 'Henry Ford', wheelbase: 100, length: 134, width:'1,676 mm' },
+    {name: 'Ford', age: 200, designer: 'Henry Ford', wheelbase: 100, length: 134, width:'1,676 mm' },
+    {name: 'Ford', age: 200, designer: 'Henry Ford', wheelbase: 100, length: 134, width:'1,676 mm' },
+    {name: 'Ford', age: 200, designer: 'Henry Ford', wheelbase: 100, length: 134, width:'1,676 mm' },
+    {name: 'Ford', age: 200, designer: 'Henry Ford', wheelbase: 100, length: 134, width:'1,676 mm' }
+]
+console.log(cars)*/
+
+
+/*Task - 3*/
+/*const house = {
+    name: 'Gulliver',
+    type: 'mixed-use',
+    location: 'Kyiv, Ukraine',
+    cost: '$200,000,000',
+    roof: true,
+    rooms: ['living room', 'kitchen', 'study'],
+    address: {
+        city: 'Kyiv',
+        country: 'Ukraine'
+    }
+}*/
+
+/*
+const driver = {
+    name: 'Andrii',
+    location: 'Kyiv, Ukraine',
+    house: true,
+    food: 'salat',
+    cars: ['Toyota', 'Volkswagen', 'Porsche'],
+    drinks: {
+        alcoholic: 'Kyiv',
+        alcoholFree: 'Ukraine'
+    }
+}
+console.log(driver)
+*/
+
+/*const toy = {
+    name: 'Cat',
+    kingdom: 'Animalia',
+    genus: 'Felis',
+    order: 'Carnivora',
+    colour: 'Black',
+    fur: true,
+    foods: ['Raw meat', 'fish', 'Chocolate'],
+    averageCatPrice: {
+        femaleCat: '$600',
+        kitten: '$300-1200'
+    }
+}
+console.log(toy);*/
+
+/*const table = {
+    long: '2.74 m',
+    wide: '1.525 m',
+    high: 76,
+    colour:'White',
+    fur: false,
+    materials: ['Fibreboard', 'Acrylic paint', 'Plastic'],
+    sustainability: {
+        leg: true,
+        tableTop: true
+    }
+}
+console.log(table);*/
+
+/*const bag = {
+    categories: 'Women',
+    designer: 'Jacquemus',
+    model: 'Chiquito',
+    colour:'White',
+    fur: false,
+    measurements: ['width 5.1 in', 'height 7.1 in', 'depth 3.1 in'],
+    size: {
+        mediumSize: true,
+        mini: false
+    }
+}
+console.log(bag);*/
+
+
+
+/*Task - 4*/
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+/*console.log(users[7].status);*/
+/*console.log(users[4].status)*/
+/*console.log(users[9].name)*/
+/*console.log(users[2].name)*/
+/*console.log(users[6].age)*/
+/*console.log(users[3].age)*/
+/*console.log(users[4].age + users[4].name)*/
+/*console.log(users[5].age + users[5].status.toString())*/
+
+
+/*Task - 5*/
+
+/*const content = document.getElementById('content');
+console.log(content.innerText);*/
+
+/*const rulesBlock = document.getElementById('rules');
+console.log(rules.innerText);*/
+
+/*
+const content = document.getElementById('content');
+content.innerText = 'kkkkkkkkkkkkkkkkkk';
+console.log(content.innerText);
+*/
+
+/*const rules = document.getElementById('rules');
+rules.innerText = 'yyyyyyyyyyyyyy';
+console.log(rules.innerText);*/
+/*
+content.style.backgroundColor = "red"
+rules.style.backgroundColor = 'red'
+
+content.style.backgroundColor = 'blue'
+rules.style.backgroundColor = 'blue'
+
+rules.classList.forEach(value => {
+    console.log(value)
+})*/
+/*const fc_rules = document.getElementsByClassName('fc_rules');
+console.log(fc_rules)*/
+
+/*const elements = document.getElementsByClassName('fc_rules');
+console.log(elements);
+for (let i= 0; i< elements.length; i+=1){
+    elements[i].style.color = 'red';
+}*/
+
+
+
+
+
+
+
+
+
+
+
